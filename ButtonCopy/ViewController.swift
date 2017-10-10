ccdefee30949f5fbf30e0e13312645f000a056c0
Dakota Weber <dakota.weber01@gmail.com>
2017-10-10 12:54:19 -0600

    Made a color changing function using a button.

--- ButtonCopy/ViewController.swift
+++ ButtonCopy/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var Button: UIButton!
     
+    @IBAction func buttonMethod(_ sender: Any)
+    {
+        Button.backgroundColor =
+        createRandomColor()
+        view.backgroundColor = createRandomColor()
         
+        Button.setTitleColor(createRandomColor(), for: .normal)
+    }
+    private func createRandomColor() -> UIColor
+        
+    { //Need RGB Values CGFloat
+        let newColor :UIColor
+        let redValue = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        let greenValue = CGFloat(Double(arc4random_uniform(256)) / 255.00)
+        let blueValue = CGFloat(Double(arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue)
+        return newColor
+    }
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
