1050b2e25758b761540a998cde2892d046409bab
Dakota Weber <dakota.weber01@gmail.com>
2017-10-16 13:11:26 -0600

    Its a whole lotta mess

---
+++ ButtonCopy/Model/ColorTools.swift
@@ -0,0 +1,21 @@
+//
+//  ColorTools.swift
+//  ButtonCopy
+//
+//  Created by Weber, Dakota on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    private lazy var colorTool : ColorTools = colorTools()
+        
+
+    {
+
+    }
+    
+
+}
---
+++ ButtonCopy/Model/ThirdScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ThirdScreenViewController.swift
+//  ButtonCopy
+//
+//  Created by Weber, Dakota on 10/16/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
